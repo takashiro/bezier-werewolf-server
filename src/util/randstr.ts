428835078833230ee7b2f0b59188b6ff281641eb
Kazuichi Takashiro <takashiro@qq.com>
2020-09-05 14:16:36 +0800

    Rewrite util functions

--- src/util/randstr.ts
+++ src/util/randstr.ts
@@ -1,18 +1,16 @@
-
 const CHARS = '`1234567890-=qwertyuiop[]\\asdfghjkl;\'zxcvbnm,./ ~!@#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:"ZXCVBNM<>?';
 
 /**
  * Generate a random key
- * @param {number} length
- * @param {string} chars Possible characters, default to all printable characters
- * @return {string}
+ * @param length
+ * @param chars Possible characters, default to all printable characters
  */
-function randstr(length, chars = CHARS) {
-	let str = [];
+function randstr(length: number, chars = CHARS): string {
+	const str = [];
 	for (let i = 0; i < length; i++) {
 		str.push(chars[Math.floor(Math.random() * chars.length)]);
 	}
 	return str.join('');
 }
 
-module.exports = randstr;
+export default randstr;
---
+++ src/util/shuffle.ts
@@ -0,0 +1,14 @@
+/**
+ * Shuffle an array in place
+ * @param a
+ */
+function shuffle<Element>(a: Element[]): void {
+	for (let i = a.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		const x = a[i];
+		a[i] = a[j];
+		a[j] = x;
+	}
+}
+
+export default shuffle;
