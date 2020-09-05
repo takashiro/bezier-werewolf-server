380ea90b7bef2bcbd2b16c3f955325aee42792d2
Kazuichi Takashiro <takashiro@qq.com>
2020-09-05 16:31:56 +0800

    Rewrite class Action

---
+++ src/game/Action.ts
@@ -0,0 +1,37 @@
+import Player from './Player';
+
+export default abstract class Action<DriverType> {
+	protected owner: Player;
+
+	protected priority: number;
+
+	/**
+	 * An action to be executed
+	 * @param owner
+	 * @param priority
+	 */
+	constructor(owner: Player, priority: number) {
+		this.owner = owner;
+		this.priority = priority;
+	}
+
+	/**
+	 * @return owner
+	 */
+	getOwner(): Player {
+		return this.owner;
+	}
+
+	/**
+	 * @return priority
+	 */
+	getPriority() {
+		return this.priority;
+	}
+
+	/**
+	 * Action effect
+	 * @param driver
+	 */
+	abstract execute(driver: DriverType): void;
+}
