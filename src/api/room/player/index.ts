1ec97098bf6069c2b9f224341b4422cabbb8696f
Kazuichi Takashiro <takashiro@qq.com>
2020-09-13 21:10:09 +0800

    Rewrite lynch API

--- src/api/room/player/index.ts
+++ src/api/room/player/index.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
 import seatRouter from './seat';
+import lynchRouter from './lynch';
 
 const router = Router({
 	mergeParams: true,
 });
 
 router.get('/:seat/seat', seatRouter);
+router.get('/:seat/lynch', lynchRouter);
 
 export default router;
---
+++ src/api/room/player/lynch.ts
@@ -0,0 +1,83 @@
+import { Router } from 'express';
+import { LynchVote } from '@bezier/werewolf-core';
+
+import DriverState from '../../../game/DriverState';
+
+import $ from './$';
+
+const router = Router({
+	mergeParams: true,
+});
+
+router.post('/', (req, res) => {
+	const context = $(req, res);
+	if (!context) {
+		return;
+	}
+
+	const target = parseInt(req.body.target, 10);
+	if (Number.isNaN(target) || target <= 0) {
+		res.status(400).send('Invalid target seat number');
+		return;
+	}
+
+	const { player, driver } = context;
+	if (driver.getState() !== DriverState.Voting) {
+		res.status(425).send('Too early to vote. Other players are still invoking their skills.');
+		return;
+	}
+
+	if (player.getLynchTarget()) {
+		res.status(409).send('You have submitted your lynch target');
+		return;
+	}
+
+	const lynchTarget = driver.getPlayer(target);
+	if (!lynchTarget) {
+		res.status(400).send('The target does not exist');
+		return;
+	}
+
+	player.setLynchTarget(lynchTarget);
+});
+
+router.get('/', (req, res) => {
+	const context = $(req, res);
+	if (!context) {
+		return;
+	}
+
+	const { driver } = context;
+	if (driver.getState() !== DriverState.Voting) {
+		res.status(425).send('Other players are still invoking their skills.');
+		return;
+	}
+
+	const playerNum = driver.getPlayers().length;
+	const players: LynchVote[] = [];
+	for (const player of driver.getPlayers()) {
+		const target = player.getLynchTarget();
+		if (target) {
+			players.push({
+				seat: player.getSeat(),
+				role: player.getRole(),
+				target: target.getSeat(),
+			});
+		}
+	}
+
+	if (players.length < playerNum) {
+		res.json({
+			progress: players.length,
+			limit: playerNum,
+		});
+	} else {
+		const cards = driver.getCenterCards();
+		res.json({
+			cards: cards.map((card) => card.toJSON()),
+			players,
+		});
+	}
+});
+
+export default router;
