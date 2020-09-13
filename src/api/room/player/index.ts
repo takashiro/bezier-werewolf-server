bec5dd9118505fddf6d9748a860964c7b2c0d05f
Kazuichi Takashiro <takashiro@qq.com>
2020-09-13 21:18:26 +0800

    Rewrite skill API

--- src/api/room/player/index.ts
+++ src/api/room/player/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import seatRouter from './seat';
+import skillRouter from './skill';
 import lynchRouter from './lynch';
 
 const router = Router({
@@ -8,6 +9,7 @@ const router = Router({
 });
 
 router.get('/:seat/seat', seatRouter);
+router.get('/:seat/skill', skillRouter);
 router.get('/:seat/lynch', lynchRouter);
 
 export default router;
---
+++ src/api/room/player/skill.ts
@@ -0,0 +1,37 @@
+import { Router } from 'express';
+
+import ProactiveSkill from '../../../game/ProactiveSkill';
+
+import $ from './$';
+
+const router = Router({
+	mergeParams: true,
+});
+
+router.post('/', (req, res) => {
+	const context = $(req, res);
+	if (!context) {
+		return;
+	}
+
+	const { player } = context;
+	const skill = player.getSkill();
+	if (!skill || !(skill instanceof ProactiveSkill)) {
+		player.setReady(true);
+		res.sendStatus(200);
+		return;
+	}
+
+	if (!skill.isInvoked(req.body)) {
+		if (!skill.isFeasible(req.body)) {
+			res.status(400).send('Invalid skill targets');
+			return;
+		}
+
+		skill.invoke(req.body);
+	}
+
+	return skill.getOutput();
+});
+
+export default router;
