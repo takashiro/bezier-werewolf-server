88630db2f6c33d188877561e3d95b549d3cda69c
Kazuichi Takashiro <takashiro@qq.com>
2020-10-08 17:49:41 +0800

    Rewrite tests for Villager

---
+++ test/collection/standard/Villager.spec.ts
@@ -0,0 +1,40 @@
+import { Player, Role } from '@bezier/werewolf-core';
+import { agent } from 'supertest';
+import app from '../../../src';
+
+const self = agent(app);
+
+const roles: Role[] = new Array(5);
+roles[0] = Role.Werewolf;
+roles.fill(Role.Villager, 1);
+
+const room = {
+	id: 0,
+	ownerKey: '',
+};
+beforeAll(async () => {
+	const res = await self.post('/room').send({ roles });
+	expect(res.status).toBe(200);
+	Object.assign(room, res.body);
+});
+
+afterAll(async () => {
+	const res = await self.delete(`/room/${room.id}?ownerKey=${encodeURIComponent(room.ownerKey)}`);
+	expect(res.status).toBe(200);
+});
+
+const players: Player[] = [];
+it('fetches all roles', async () => {
+	for (let seat = 1; seat <= roles.length - 3; seat++) {
+		const res = await self.get(`/room/${room.id}/player/${seat}/seat?seatKey=${seat}`);
+		expect(res.status).toBe(200);
+		players.push(res.body);
+	}
+});
+
+it('gets ready', async () => {
+	for (const player of players) {
+		const res = await self.post(`/room/${room.id}/player/${player.seat}/skill?seatKey=${player.seat}`);
+		expect(res.status).toBe(200);
+	}
+});
