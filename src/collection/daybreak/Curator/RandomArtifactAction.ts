d4b5555aa19a1abdc118ace90b27cf050e7f32ff
Kazuichi Takashiro <takashiro@qq.com>
2021-01-16 15:58:25 +0800

    Add class RandomArtifactAction

---
+++ src/collection/daybreak/Curator/RandomArtifactAction.ts
@@ -0,0 +1,30 @@
+import { Artifact } from '@bezier/werewolf-core';
+import Action from '../../../game/Action';
+import Player, { Skill } from '../../../game/Player';
+
+
+const artifacts: Artifact[] = [
+	Artifact.VoidOfNothingness,
+	Artifact.ShroudOfShame,
+	Artifact.MaskOfMuting,
+	Artifact.ClawOfTheWerewolf,
+	Artifact.BrandOfTheVillager,
+	Artifact.CudgelOfTheTanner,
+	Artifact.BowOfTheHunter,
+	Artifact.SwordOfTheBodyguard,
+	Artifact.CloakOfThePrince,
+];
+
+export default class RandomArtifactAction extends Action {
+	protected target: Player;
+
+	constructor(skill: Skill, target: Player) {
+		super(skill);
+		this.target = target;
+	}
+
+	protected run(): void {
+		const artifact = artifacts[Math.floor(Math.random() * artifacts.length)];
+		this.target.addArtifact(artifact);
+	}
+}
---
+++ src/collection/daybreak/Curator/index.ts
@@ -0,0 +1,25 @@
+import { Selection } from '@bezier/werewolf-core';
+
+import Skill from '../../Skill';
+import MutexType from '../../../game/MutexType';
+import SkipAction from '../../SkipAction';
+import RandomArtifactAction from './RandomArtifactAction';
+
+export default class Curator extends Skill<void> {
+	protected priority = 0xb00;
+
+	protected writeMode = [MutexType.Any];
+
+	isFeasible(data: Selection): boolean {
+		const player = this.selectPlayer(data);
+		return Boolean(player && player.getArtifactNum() <= 0);
+	}
+	protected run(data: Selection): void {
+		const player = this.selectPlayer(data);
+		if (!player) {
+			this.driver.addAction(new SkipAction(this));
+		} else {
+			this.driver.addAction(new RandomArtifactAction(this, player));
+		}
+	}
+}
