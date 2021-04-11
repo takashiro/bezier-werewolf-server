854395efe98968cf4010d9e4a16264d543d037d6
Kazuichi Takashiro <takashiro@qq.com>
2021-04-11 23:06:30 +0800

    Update unit tests of Minion

--- test/collection/standard/Minion.spec.ts
+++ test/collection/standard/Minion.spec.ts
@@ -1,8 +1,5 @@
 import {
-	Player,
 	Role,
-	Team,
-	Teamship,
 	Vision,
 } from '@bezier/werewolf-core';
 import { agent } from 'supertest';
@@ -10,20 +7,29 @@ import app from '../../../src';
 
 const self = agent(app);
 
-const roles: Role[] = new Array(50);
-roles.fill(Role.Werewolf, 0, 5);
-roles.fill(Role.AlphaWolf, 5, 10);
-roles.fill(Role.Minion, 10, 15);
-for (let i = 15; i < 50; i++) {
-	roles[i] = 1000 + i;
-}
+const roles: Role[] = [
+	1001,
+	1002,
+	1003,
+	Role.Werewolf,
+	Role.DreamWolf,
+	Role.Minion,
+	Role.Robber,
+];
+
+const playerNum = roles.length - 3;
+const minion = 3;
+const robber = 4;
 
 const room = {
 	id: 0,
 	ownerKey: '',
 };
 beforeAll(async () => {
-	const res = await self.post('/room').send({ roles });
+	const res = await self.post('/room').send({
+		roles,
+		random: false,
+	});
 	expect(res.status).toBe(200);
 	Object.assign(room, res.body);
 });
@@ -33,33 +39,30 @@ afterAll(async () => {
 	expect(res.status).toBe(200);
 });
 
-const players: Player[] = [];
 it('fetches all roles', async () => {
-	const playerNum = roles.length - 3;
 	for (let seat = 1; seat <= playerNum; seat++) {
 		const res = await self.get(`/room/${room.id}/player/${seat}/seat?seatKey=1`);
 		expect(res.status).toBe(200);
-		players.push(res.body);
 	}
 });
 
+it('can act after Robber does', async () => {
+	const res = await self.post(`/room/${room.id}/player/${robber}/skill?seatKey=1`)
+		.send({ players: [2] });
+	expect(res.status).toBe(200);
+	const { players } = res.body as Vision;
+	expect(players[0].role).toBe(Role.DreamWolf);
+});
+
 it('sees all werewolves', async () => {
-	const werewolves = players.filter((player) => player.role !== Role.Minion && Teamship.get(player.role) === Team.Werewolf);
-	const minions = players.filter((player) => player.role === Role.Minion);
-	for (const minion of minions) {
-		const res = await self.post(`/room/${room.id}/player/${minion.seat}/skill?seatKey=1`);
-		expect(res.status).toBe(200);
-		const vision: Vision = res.body;
+	const res = await self.post(`/room/${room.id}/player/${minion}/skill?seatKey=1`);
+	expect(res.status).toBe(200);
+	const { cards, players } = res.body as Vision;
 
-		const seen = vision.players.map((player) => player.seat);
-		expect(seen).toHaveLength(werewolves.length);
-		for (const werewolf of werewolves) {
-			expect(seen).toContain(werewolf.seat);
-		}
+	expect(cards).toBeUndefined();
 
-		expect(werewolves.some((werewolf) => werewolf.role !== Role.Werewolf)).toBe(true);
-		for (const player of vision.players) {
-			expect(player.role).toBe(Role.Werewolf);
-		}
-	}
+	expect(players[0].role).toBe(Role.Werewolf);
+	expect(players[0].seat).toBe(1);
+	expect(players[1].role).toBe(Role.Werewolf);
+	expect(players[1].seat).toBe(2);
 });
