ce27c416d59e720b23b3a4359ce2e8ddecc470e3
Kazuichi Takashiro <takashiro@qq.com>
2021-05-05 10:59:03 +0800

    Test class Room

---
+++ test/base/Room.spec.ts
@@ -0,0 +1,19 @@
+import Room from '../../src/base/Room';
+
+const room = new Room();
+
+it('generates random keys', () => {
+	expect(room.getSalt()).toHaveLength(8);
+	expect(room.getOwnerKey()).toHaveLength(32);
+});
+
+it('generates JSON representation', () => {
+	expect(room.toJSON()).toStrictEqual({
+		id: 0,
+		salt: room.getSalt(),
+		cardNum: 0,
+		roles: [],
+		random: true,
+		loneWolf: false,
+	});
+});
