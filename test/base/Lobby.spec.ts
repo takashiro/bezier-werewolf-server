37310c1eea3c6a62b6279524562d66053c429e5e
Kazuichi Takashiro <takashiro@qq.com>
2021-05-05 10:54:00 +0800

    Fully test class Lobby

--- test/base/Lobby.spec.ts
+++ test/base/Lobby.spec.ts
@@ -7,16 +7,27 @@ it('has a capacity of 1000 by default', () => {
 	expect(lobby.getCapacity()).toBe(1000);
 });
 
+it('changes capacity', () => {
+	lobby.setCapacity(100);
+	expect(lobby.getCapacity()).toBe(100);
+});
+
+it('changes expiry', () => {
+	lobby.setRoomExpiry(10);
+	expect(lobby.getRoomExpiry()).toBe(10);
+});
+
 it('generates random room number', () => {
 	const room = new Room();
 	lobby.add(room);
 	expect(room.getId()).toBeGreaterThan(0);
-	lobby.remove(room.getId());
+	expect(lobby.remove(room.getId())).toBe(true);
+	expect(lobby.remove(room.getId())).toBe(false);
 });
 
 it('generates massive rooms', () => {
 	const ids = new Set<number>();
-	for (let i = 0; i < 1000; i++) {
+	for (let i = 0; i < 100; i++) {
 		const room = new Room();
 		lobby.add(room);
 		ids.add(room.getId());
@@ -25,10 +36,11 @@ it('generates massive rooms', () => {
 	const fin = new Room();
 	expect(lobby.add(fin)).toBe(false);
 	expect(fin.getId()).toBe(0);
+	expect(ids.size).toBe(100);
+});
 
-	for (const id of ids) {
-		lobby.remove(id);
-	}
-
-	expect(ids.size).toBe(1000);
+it('automatically removes expired rooms', async () => {
+	await new Promise((resolve) => setTimeout(resolve, 20));
+	const status = lobby.getStatus();
+	expect(status.roomNum).toBe(0);
 });
